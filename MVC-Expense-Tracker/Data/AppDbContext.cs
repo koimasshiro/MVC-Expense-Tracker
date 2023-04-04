68cbafbb12213d2f52e1078ecb2ab7e3b4b84edb
koimasshiro <ese.imhariagbe@gmail.com>
2023-04-04 12:36:56 +0100

    first commit

---
+++ MVC-Expense-Tracker/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Expense_Tracker.Models;
+
+namespace MVC_Expense_Tracker.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) 
+        {
+        }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
